2a83ba6a4770374275b4e6678b87161065a4b01a
Vladimir Tsyganov <tsyganov.vv0228@gmail.com>
2026-01-29 15:06:09 +0700

    update docker + config
    docker db + smtp server + config /.env for smtp

--- apps/api.mind-spark.ru/app/core/config.py
+++ apps/api.mind-spark.ru/app/core/config.py
@@ -5,6 +5,11 @@ class Settings(BaseSettings):
     # Database
     DATABASE_URL: str
 
+    SMTP_SERVER: str 
+    SMTP_PORT: str
+    EMAIL_ADDRESS: str
+    EMAIL_PASSWORD: str
+    
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=True,
     )
---
+++ apps/docker/smtp.Dockerfile
@@ -0,0 +1,4 @@
+FROM maildev/maildev:latest
+
+EXPOSE 1025
+EXPOSE 1080
