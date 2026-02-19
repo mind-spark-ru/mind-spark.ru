107656168d8a47cf7236fb1de1e6448c9011872b
Maria Popova <mariiappp456ova@gmail.com>
2026-02-19 19:03:29 +0700

    Main screen

--- apps/react-native.mind-spark.ru/src/hooks/useAppFonts.jsx
+++ apps/react-native.mind-spark.ru/src/hooks/useAppFonts.jsx
@@ -4,6 +4,7 @@ export const useAppFonts = () => {
   const [fontsLoaded, fontError] = useFonts({
     'Montserrat-SemiBold': require('@assets/fonts/Montserrat-SemiBold.ttf'),
     'Montserrat-Regular': require('@assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Italic': require('@assets/fonts/Montserrat-Italic.ttf'),
   });
 
   return { fontsLoaded, fontError };
