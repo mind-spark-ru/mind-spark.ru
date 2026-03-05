5f4aaaddb7f3164f18d650ac60db310c50fd6880
Vladimir Tsyganov <tsyganov.vv0228@gmail.com>
2026-03-06 00:43:40 +0700

    fix api url prod

--- apps/react-native.mind-spark.ru/config.js
+++ apps/react-native.mind-spark.ru/config.js
@@ -1,4 +1,6 @@
 import Constants from 'expo-constants';
 
-export const API_URL =`http://${Constants.expoConfig?.hostUri?.split(':')[0]}:8000`;
-export const REACT_URL =`http://${Constants.expoConfig?.hostUri?.split(':')[0]}:3000`;
+export const API_URL = `http://mind-spark.ru:8000`
+export const REACT_URL = `http://mind-spark.ru:8000`
+export const API_URL_dev =`http://${Constants.expoConfig?.hostUri?.split(':')[0]}:8000`;
+export const REACT_URL_dev =`http://${Constants.expoConfig?.hostUri?.split(':')[0]}:3000`;
