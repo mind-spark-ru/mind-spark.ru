76baf30314d6108f26c36122a356c670a044d69c
Vladimir Tsyganov <tsyganov.vv0228@gmail.com>
2026-03-06 01:19:27 +0700

    fix configs

--- apps/react-native.mind-spark.ru/config.js
+++ apps/react-native.mind-spark.ru/config.js
@@ -1,6 +1,6 @@
 import Constants from 'expo-constants';
 
-export const API_URL = `http://mind-spark.ru:8000`
-export const REACT_URL = `http://mind-spark.ru:3000`
+export const API_URL = `http://mind-spark.ru/api`
+export const REACT_URL = `http://mind-spark.ru`
 export const API_URL_dev =`http://${Constants.expoConfig?.hostUri?.split(':')[0]}:8000`;
 export const REACT_URL_dev =`http://${Constants.expoConfig?.hostUri?.split(':')[0]}:3000`;
